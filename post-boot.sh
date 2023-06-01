821fa00d48f2099cb02873b9e1a3d592c8047531
Heng Zhou <111557423+Heng-Zhou@users.noreply.github.com>
2023-06-01 14:51:05 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -112,6 +112,7 @@ flash_card() {
     echo "Flash Card(s). "
     if [[ "$TOOLVERSION" == "2022.1" ]] || [[ "$TOOLVERSION" == "2022.2" ]]; then
         #/opt/xilinx/xrt/bin/xbmgmt program --base --device 0000:3b:00.0
+        :
     else
         /opt/xilinx/xrt/bin/xbmgmt flash --update --shell $DSA --force
     fi
