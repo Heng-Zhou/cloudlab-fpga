7c7fe121e2653fa11d5ba98666601ab373e28dc2
Heng Zhou <111557423+Heng-Zhou@users.noreply.github.com>
2023-06-02 00:35:33 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -31,6 +31,7 @@ install_xrt() {
         echo "Installing XRT package..."
         sudo yum install -y /tmp/$XRT_PACKAGE
     fi
+    rm /tmp/$XRT_PACKAGE
     sudo bash -c "echo 'source /opt/xilinx/xrt/setup.sh' >> /etc/profile"
 }
 
